272500bcffca99f9cbfcb282d5a5081d70fbc6bc
aitoAarni <aarni.rechardt@gmail.com>
2026-01-19 19:22:23 +0100

    tokenizer works with integers

--- src/compiler/test_tokenizer.py
+++ src/compiler/test_tokenizer.py
@@ -5,3 +5,6 @@ def test_empty_input() -> None:
 
 def test_ignore_white_space() -> None:
     assert tokenizer("  \n\t") == []
+
+def test_tokenizes_positive_integers() -> None:
+    assert tokenizer("12 34\t56\n78   9") == ["12", "34", "56", "78", "9"]
--- src/compiler/tokenizer.py
+++ src/compiler/tokenizer.py
@@ -1,7 +1,7 @@
 import re
 
 def tokenizer(source_code: str="") -> list[str]:
-    r = re.compile(r"[^\n\t ]*")
+    r = re.compile(r"[1-9]*")
 
     unfiltered_tokens: list[str] = r.findall(source_code)
     tokens = list(filter(bool, unfiltered_tokens))
