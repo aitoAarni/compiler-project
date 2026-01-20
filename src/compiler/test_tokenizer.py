fa6de4f0f46aa91e41948bcb48f16420b01e26a3
aitoAarni <aarni.rechardt@gmail.com>
2026-01-20 17:48:30 +0100

    Task 1 complete

--- src/compiler/test_tokenizer.py
+++ src/compiler/test_tokenizer.py
@@ -1,3 +1,4 @@
+import pytest
 from .tokenizer import tokenizer
 
 def test_empty_input() -> None:
@@ -10,6 +11,12 @@ def test_tokenizes_positive_integers() -> None:
     assert tokenizer("12 34\t56\n78   9") == ["12", "34", "56", "78", "9"]
 
 def test_tokenizes_identifiers() -> None:
-    assert tokenizer("if \nwhile _var1 2not_ok .not_ok\nOk_2") == [
+    assert tokenizer("if \nwhile _var1\nOk_2") == [
         "if", "while", "_var1", "Ok_2"
         ]
+
+def test_throws_error_on_invalid_identifiers() -> None:
+    with pytest.raises(Exception, match="Invalid syntax. Could not tokenize 2var"):
+        tokenizer("2var")
+    with pytest.raises(Exception, match="Invalid syntax. Could not tokenize \\.var"):
+        tokenizer(".var")
--- src/compiler/tokenizer.py
+++ src/compiler/tokenizer.py
@@ -1,29 +1,49 @@
 import re
 
-def add_whitespace_match(regex: str) -> str:
-    white_space = r"[\n|\t| ]"
-    return white_space + regex + white_space
+
+def tokenizer(source_code: str="") -> list[str]:
+    identifier = get_regex_for_token("identifier")
+    positive_integer = get_regex_for_token("positive_integer")
+    white_space = get_regex_for_token("white_space")
+
+    identifier_re = re.compile(identifier)
+    positive_integer_re = re.compile(positive_integer)
+    white_space_re = re.compile(white_space)
+    regular_expressions = [identifier_re, positive_integer_re, white_space_re]
+    previous_match_end = 0
+    tokens: list[str] = []
+    require_white_space_after = False
+
+    while (previous_match_end < len(source_code)):
+        for regex in regular_expressions:
+            match = regex.search(source_code, previous_match_end)
+            print("match:", match)
+            if match and match.start() == previous_match_end:
+                print("match accepted")
+                previous_match_end = match.end()
+                if regex == white_space_re:
+                    print("in white space regex")
+                    require_white_space_after = False
+                elif require_white_space_after:
+                    raise Exception(f"Invalid syntax. Could not tokenize {source_code}")
+                elif regex == identifier_re:
+                    tokens.append(match[0])
+                elif regex == positive_integer_re:
+                    require_white_space_after = True
+                    tokens.append(match[0])
+                break
+        else:
+            raise Exception(f"Invalid syntax. Could not tokenize {source_code}")
+
+    return tokens
 
 def get_regex_for_token(regex: str) -> str:
     tokenizer_regexes = {
         "identifier": r"[a-zA-Z|_][a-zA-Z|_|0-9]*",
         "positive_integer" : r"[0-9]+",
+        "white_space" : r"[\n|\t| ]+"
     }
-    return add_whitespace_match(tokenizer_regexes[regex])
+    return tokenizer_regexes[regex]
     
-
-def tokenizer(source_code: str="") -> list[str]:
-    identifier = get_regex_for_token("identifier")
-    positive_integer = get_regex_for_token("positive_integer")
-    r = re.compile(rf"{identifier}|{positive_integer}")
-    source_code_spaced =  " " + source_code + " "
-    previous_match_end = 0
-    tokens: list[str] = []
-    while (match := r.search(source_code_spaced, previous_match_end)):
-        
-        previous_match_end = match.end() - 1
-        tokens.append(match[0].strip())
-    return tokens
-
 if __name__ == "__main__":
-    print(tokenizer("if \nwhile _var1 2not_ok .not_ok\nOk_2"))
+    print(tokenizer("if \nwhile _var1 not_ok not_ok\nOk_2"))
