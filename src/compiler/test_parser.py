2e7a1dc40638ce03477a961f9dca1c5c0053c95c
aitoAarni <aarni.rechardt@gmail.com>
2026-01-27 17:30:05 +0200

    modify test

--- src/compiler/test_parser.py
+++ src/compiler/test_parser.py
@@ -6,6 +6,7 @@ import compiler.custom_ast as ast
 
 t = ["int_literal", "identifier", "punctuation"]
 
+
 def create_tokens(*token_args: Sequence[str | int]) -> list[Token]:
     tokens = []
     for args in token_args:
@@ -61,37 +62,46 @@ def test_parse_term():
     parsed = parse(tokens)
     assert parsed == correct_expresion
 
+
 def test_parse_with_parenthesis():
     a = ast.Identifier("a")
     tokens = create_tokens(
-        ["(", t[2]],["a", t[1]], ["+", t[1]], ["a", t[1]], [")", t[2]],
-        ["*", t[1]], [2, t[0]] 
+        ["(", t[2]],
+        ["a", t[1]],
+        ["+", t[1]],
+        ["a", t[1]],
+        [")", t[2]],
+        ["*", t[1]],
+        [2, t[0]],
     )
     correct_expresion = ast.BinaryOp(ast.BinaryOp(a, "+", a), "*", ast.Literal(2))
     parsed = parse(tokens)
     assert parsed == correct_expresion
 
+
 def test_empty_input_on_parser():
     parsed = parse([])
     assert parsed == None
 
+
 def test_invalid_operation():
     tokens = create_tokens([2, t[0]], ["+", t[1]])
-    with pytest.raises(Exception, match=
-                       r"SourceLocation\(line=0, column=0\):"
-                       r" expected \"\(\", an integer literal or an identifier"
-                       ):
+    with pytest.raises(
+        Exception,
+        match=r"SourceLocation\(line=0, column=0\):"
+        r" expected \"\(\", an integer literal or an identifier",
+    ):
         print(parse(tokens))
+
 
 def test_ternary_operator():
     two = ast.Literal(2)
     three = ast.Literal(3)
     right_answer = ast.TernaryOp(two, three, None)
-    tokens = create_tokens(
-        ["if", t[1]], [2, t[0]], ["then", t[1]], [3, t[0]]
-    )
+    tokens = create_tokens(["if", t[1]], [2, t[0]], ["then", t[1]], [3, t[0]])
     parsed = parse(tokens)
     assert parsed == right_answer
+
 
 def test_ternary_operator_with_else():
     if_ = ast.Literal(1)
@@ -100,22 +110,33 @@ def test_ternary_operator_with_else():
 
     right_answer = ast.TernaryOp(if_, then_, else_)
     tokens = create_tokens(
-        ["if", t[1]], [1, t[0]], ["then", t[1]], [2, t[0]],
-        ["else", t[1]], [3, t[0]]
+        ["if", t[1]], [1, t[0]], ["then", t[1]], [2, t[0]], ["else", t[1]], [3, t[0]]
     )
     parsed = parse(tokens)
     assert parsed == right_answer
+
 
 def test_ternary_operator_expressions():
     expr1 = ast.BinaryOp(ast.Literal(2), "-", ast.Identifier("a"))
     expr2 = ast.BinaryOp(ast.Literal(3), "+", ast.Literal(4))
     expr3 = ast.Identifier("b")
-    correct_answer = ast.TernaryOp(expr1, expr2, expr3)
+    correct_answer = ast.BinaryOp(
+        ast.Literal(2), "*", ast.TernaryOp(expr1, expr2, expr3)
+    )
 
     tokens = create_tokens(
-        ["if", t[1]], ["2", t[0]], ["-", t[1]], ["a", t[1]],
-        ["then", t[1]], [3, t[0]], ["+", t[1]], [4, t[0]],
-        ["else", t[1]], ["b", t[1]]
+        [2, t[0]],
+        ["*", t[1]],
+        ["if", t[1]],
+        ["2", t[0]],
+        ["-", t[1]],
+        ["a", t[1]],
+        ["then", t[1]],
+        [3, t[0]],
+        ["+", t[1]],
+        [4, t[0]],
+        ["else", t[1]],
+        ["b", t[1]],
     )
     parsed = parse(tokens)
     assert parsed == correct_answer
